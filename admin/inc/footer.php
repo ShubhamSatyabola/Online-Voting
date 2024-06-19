2df248d55669bacb137ddd2c1ac9b688897363d6
ShubhamSatyabola <satyabolashubham@gmail.com>
2024-06-19 15:02:11 +0530

    voting system

---
+++ admin/inc/footer.php
@@ -0,0 +1,15 @@
+
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p> &copy; A voting App for learning purpose<br />
+                Developed by Shubham Satyabola
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
